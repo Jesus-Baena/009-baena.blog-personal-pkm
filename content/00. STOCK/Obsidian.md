d4de525e7ae77ca93a5db285e10feb7337c326d6
Jesús Baena <me@jbaena.net>
2025-08-12 11:17:37 +0300

    Appareance changes

--- content/00. STOCK/Obsidian.md
+++ content/00. STOCK/Obsidian.md
@@ -9,4 +9,10 @@ This blog is a subset of my complete Obsidian setup. This is a screenshot of how
 
 ![[Pasted image 20250812084447.png]]
 
-I have tried many other systems: notion, heptabase, Logsec, Anytype. All fine solutions, but I always end up returning to Obsidian. 
+I have tried many other systems: Notion, Heptabase, Logsec, Anytype... All fine solutions, but I always end up returning to Obsidian. 
+
+For the **Syncing** of my notes across devices, I am using the official service provided by Obsidian. Paradoxically, the place where I develop my self-hosted projects, is not selfhosted. 
+
+For the **Publishing** of this blog, I am using an open source project [Quartz](https://quartz.jzhao.xyz/). I only made minor changes that can be copied on [my public repository](https://github.com/Jesus-Baena/009-baena.blog-personal-pkm) on GitHub. I have changed the footer and the filtering of the explorer. 
+
+For the **Hosting**, I am using Cloudflare Pages. The easiness of deployment using Workers is unbeatable. I commit to my GitHub repository and automatically is picked up by the Cloudflare Worker to build and serve. 
--- quartz/components/PageTitle.tsx
+++ quartz/components/PageTitle.tsx
@@ -1,4 +1,4 @@
-import { pathToRoot } from "../util/path"
+import { pathToRoot, joinSegments } from "../util/path"
 import { QuartzComponent, QuartzComponentConstructor, QuartzComponentProps } from "./types"
 import { classNames } from "../util/lang"
 import { i18n } from "../i18n"
@@ -6,14 +6,40 @@ import { i18n } from "../i18n"
 const PageTitle: QuartzComponent = ({ fileData, cfg, displayClass }: QuartzComponentProps) => {
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
+  const logoPath = joinSegments(baseDir, "static/jbaena-logo-length.png")
   return (
-    <h2 class={classNames(displayClass, "page-title")}>
-      <a href={baseDir}>{title}</a>
-    </h2>
+    <div class={classNames(displayClass, "page-title-container")}>
+      <a href={baseDir} class="page-title-link">
+        <img src={logoPath} alt="Logo" class="page-logo" />
+        <h2 class="page-title">{title}</h2>
+      </a>
+    </div>
   )
 }
 
 PageTitle.css = `
+.page-title-container {
+  margin: 0;
+  text-align: center;
+}
+
+.page-title-link {
+  text-decoration: none;
+  color: inherit;
+  display: block;
+}
+
+.page-title-link:hover {
+  text-decoration: none;
+}
+
+.page-logo {
+  max-width: 200px;
+  height: auto;
+  margin: 0 auto 0.5rem auto;
+  display: block;
+}
+
 .page-title {
   font-size: 1.75rem;
   margin: 0;
